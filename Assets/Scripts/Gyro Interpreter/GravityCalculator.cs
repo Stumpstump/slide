9101040ee99b953fc513609d4155d57c40f13226
taylor hocutt <taylor.hocutt@gmail.com>
2015-09-12 15:14:09 -0400

    so far attempts to implement gyro/accelerometer have failed

---
+++ Assets/Scripts/Gyro Interpreter/GravityCalculator.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using MSP_Input;
+
+public class GravityCalculator : MonoBehaviour 
+{
+	//public GyroAccel gyroAccel;
+
+	public Text x;
+	public Text y;
+	public Text z;
+	public Text w;
+
+	public Text x1;
+	public Text y1;
+	public Text z1;
+
+	public Text x2;
+	public Text y2;
+	public Text z2;
+
+	public Rigidbody2D rigidBody;
+	public float speed = 9.86f;
+
+	private void FixedUpdte () 
+	{
+		//if (pitch != null) pitch.text = gyroAccel.pitch.ToString ();
+		//if (roll != null) roll.text = gyroAccel.roll.ToString ();
+		//if (heading != null) heading.text = gyroAccel.heading.ToString ();
+		//transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
+
+		x.text = Input.gyro.attitude.x.ToString ();
+		y.text = Input.gyro.attitude.y.ToString ();
+		z.text = Input.gyro.attitude.z.ToString ();
+		w.text = Input.gyro.attitude.w.ToString ();
+
+		x1.text = Input.acceleration.x.ToString ();
+		y1.text = Input.acceleration.y.ToString ();
+		z1.text = Input.acceleration.z.ToString ();
+
+		x2.text = Input.gyro.gravity.x.ToString ();
+		y2.text = Input.gyro.gravity.y.ToString ();
+		z2.text = Input.gyro.gravity.z.ToString ();
+	}
+}
